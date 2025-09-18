315e0023514e12e58250856cb4ee5734d7874172
Rasheda-hub <jawadilrasheda@gmail.com>
2025-09-18 13:49:52 +0800

    Initial FastAPI app

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# main.py
 from fastapi import FastAPI
-from routers import message_router
 
 app = FastAPI()
 
-# Attach your router
-app.include_router(message_router.router)
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Render!"}
